a84979dc006c23824acebb30a1f45aca7bdfc937
accelsao <jay60550@yahoo.com.tw>
2018-11-19 17:18:05 +0800

    v0

---
+++ B&B.asm
@@ -0,0 +1,28 @@
+TITLE Example of ASM                (asmExample.ASM)
+
+; This program locates the cursor and displays the
+; system time. It uses two Win32 API structures.
+; Last update: 6/30/2005
+
+INCLUDE Irvine32.inc
+
+; Redefine external symbols for convenience
+; Redifinition is necessary for using stdcall in .model directive 
+; using "start" is because for linking to WinDbg.  added by Huang
+ 
+main          EQU start@0
+
+.data
+
+.code
+
+
+main PROC
+	mov eax,5
+	
+	
+	
+main ENDP
+
+
+END main
